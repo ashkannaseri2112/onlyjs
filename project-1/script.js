e3521493563a9b2991822e6b1f3276dae8dd81cf
Ashkan <141755372+ashkannaseri2112@users.noreply.github.com>
2024-05-17 14:30:11 +0330

    feature

--- project-1/script.js
+++ project-1/script.js
@@ -1,3 +1,8 @@
-const login = max => (function(produce){
- return produce;
-}) 
+const login = (max) =>
+  function (produce) {
+    return produce;
+  };
+
+const feature = function (call) {
+  return (call = component);
+};
